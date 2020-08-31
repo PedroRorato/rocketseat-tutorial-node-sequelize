b3ce35f49f371ef9cbd5a63411b42c1446c2d5d0
Pedro <pedroberorato@gmail.com>
2020-08-31 15:49:51 -0300

    Validaçao do Usuario

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -11,8 +11,12 @@ module.exports = {
     async store(request, response) {
         const { name, email } = request.body;
 
-        const user = await User.create({ name, email });
-
-        return response.json(user);
+        try {
+            const user = await User.create({ name, email });
+            return response.json(user);
+        } catch(err) {
+            return response.status(400).send({ error: err });
+        }
+        
     }
 };
--- src/models/User.js
+++ src/models/User.js
@@ -3,8 +3,31 @@ const { Model, DataTypes } = require('sequelize');
 class User extends Model {
     static init(connection) {
         super.init({
-            name: DataTypes.STRING,
-            email: DataTypes.STRING,
+            name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    notEmpty: {
+                        msg: "Esse campo não pode ser vazio"
+                    },
+                    len: {
+                        args: [4, 20],
+                        msg: "Esse campo deve ter entre 4 e 20 caracteres"
+                    }
+                }
+            },
+            email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: {
+                    isEmail: {
+                        msg: "Formato de email inválido, por favor confira o email informado!"
+                    },
+                    notEmpty: {
+                        msg: "Esse campo não pode ser vazio"
+                    }
+                }
+            },
         }, {
             sequelize: connection,
         })
